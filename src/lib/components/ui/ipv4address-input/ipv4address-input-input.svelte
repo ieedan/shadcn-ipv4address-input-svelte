4925a0c8828a5c6f373769f1fc3f9d12d233b697
Aidan Bleser <aidanbleser35@gmail.com>
2024-11-03 06:19:10 -0600

    arrow keys

--- src/lib/components/ui/ipv4address-input/ipv4address-input-input.svelte
+++ src/lib/components/ui/ipv4address-input/ipv4address-input-input.svelte
@@ -2,15 +2,17 @@
 	import { isNumber } from '.';
 
 	type Props = {
-		value?: number | null;
+		value?: string | null;
 		goNext?: () => void;
 		goPrevious?: () => void;
-		ref?: HTMLInputElement
+		ref?: HTMLInputElement;
 	};
 
 	let { value = $bindable(null), goPrevious, goNext, ref = $bindable() }: Props = $props();
 
 	const onInput = (e: KeyboardEvent) => {
+		if (e.ctrlKey || e.metaKey) return;
+
 		// just continue as normal
 		if (e.key == 'Tab' || e.key == 'Delete') return;
 
@@ -30,7 +32,29 @@
 			return;
 		}
 
+		const target = e.target as HTMLInputElement;
+
+		if (e.key == 'ArrowRight') {
+			// only go to next box if at end
+			if (target.selectionStart == target.value.length) {
+				e.preventDefault();
+				goNext?.();
+			}
+			return;
+		}
+
+		if (e.key == 'ArrowLeft') {
+			// only go to previous box if at start
+			if (target.selectionStart == 0) {
+				e.preventDefault();
+				goPrevious?.();
+			}
+			return;
+		}
+
 		// disallow any non numbers
+		// By default this prevents any undefined behavior 
+		// so make sure anything that can happen is defined.
 		if (!isNumber(e.key)) {
 			e.preventDefault();
 			return;
@@ -77,7 +101,7 @@
 	maxlength={3}
 	bind:value
 	onkeydown={onInput}
-	type="number"
+	type="string"
 	class="hide-ramp h-full w-9 border-0 bg-transparent text-center outline-none focus:outline-none"
 />
 
